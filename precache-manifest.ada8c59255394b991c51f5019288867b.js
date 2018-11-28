42ba40df6b38cc80605b6d494ca3f2b74c464cd2
Colombe <c.schh@outlook.com>
2018-11-28 19:57:59 +0100

    Updates

---
+++ precache-manifest.ada8c59255394b991c51f5019288867b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/happydrink/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8b09ec0c47252957aec2",
+    "url": "/happydrink/static/js/runtime~main.8b09ec0c.js"
+  },
+  {
+    "revision": "bc754d7998cc68d1be2f",
+    "url": "/happydrink/static/js/main.bc754d79.chunk.js"
+  },
+  {
+    "revision": "b6820fdb212e9dbdbc22",
+    "url": "/happydrink/static/js/1.b6820fdb.chunk.js"
+  },
+  {
+    "revision": "bc754d7998cc68d1be2f",
+    "url": "/happydrink/static/css/main.1991d1d4.chunk.css"
+  },
+  {
+    "revision": "3aa74b7418ddb36d10cd10ea41af582a",
+    "url": "/happydrink/index.html"
+  }
+];
